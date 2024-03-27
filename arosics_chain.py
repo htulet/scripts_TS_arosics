2474a657a8a3210892c3648f6ada5a12daba034c
htulet <hadrien.tulet@cirad.fr>
2024-03-27 14:52:11 +0100

    arosics : plot option and no subprocess

--- arosics_chain.py
+++ arosics_chain.py
@@ -57,45 +57,31 @@ def harmonize_crs(input_path, ref_path, check_ref=True):
         ds_out.write(img_in)
         ds_out.close()
 
-def call_arosics(path_in, path_ref, path_out, corr_type = 'global', max_shift=250, max_iter=100, window_size=None, mp=0, grid_res=1000):
-    assert corr_type in ['global', 'local']
-    if corr_type == 'global':
-        if window_size is None :
-            window_size = 1500
-        grid_res = ""
-    elif corr_type == 'local':
-        if window_size is None :
-            window_size = 4000 
-    command = f"arosics {corr_type} -max_shift {max_shift} -max_iter {max_iter} -ws {window_size} {window_size} -mp {mp} -fmt_out GTIFF -o {path_out} {path_ref} {path_in} {grid_res}"
-    print(command)
-    subprocess.run(command, shell=True)
+def call_arosics(path_in, path_ref, path_out, corr_type = 'global', max_shift=250, max_iter=100, window_size=1500, mp=0, grid_res=1000, save_csv = True, save_vector_plot = False):
+    CPUs = None if mp else 1
+    if corr_type=='global':
+        CR = COREG(path_ref, path_in, path_out=path_out, fmt_out="GTIFF", ws=(window_size, window_size), max_shift=max_shift, max_iter=max_iter, CPUs=CPUs)
+        CR.correct_shifts()
+    elif corr_type=='local':
+        CR = COREG_LOCAL(path_ref, path_in, path_out=path_out, fmt_out="GTIFF", window_size=(window_size, window_size), max_shift=max_shift, max_iter=max_iter, CPUs=CPUs, grid_res=grid_res)
+        CR.correct_shifts()
+        if save_csv:
+            df = CR.CoRegPoints_table
+            df.to_csv(path_out.split('.')[0] + '_CoRegPoints_table.csv')
+        if save_vector_plot:
+            DPI=300
+            vector_scale=15
+            CR.view_CoRegPoints(shapes2plot = 'vectors', savefigPath = path_out.split('.')[0] + f"_vector_map_{DPI}DPI_{vector_scale}.JPEG", savefigDPI=300, vector_scale=vector_scale, backgroundIm='tgt')
+    return CR
 
-def complete_arosics_process_v1(path_in, ref_filepath, out_dir_path, corr_type = 'global', dynamic_corr = False):
-    dynamic_corr = str2bool(dynamic_corr)
-    extensions = ('.tif', '.tiff', '.TIF', '.TIFF')
-    if os.path.isfile(path_in):
-        harmonize_crs(path_in, ref_filepath)
-        path_out = os.path.join(out_dir_path, path_in.split('/')[-1].split('\\')[-1].split('.')[0] + '_aligned.tif')
-        call_arosics(path_in, ref_filepath, path_out, corr_type)
-    elif os.path.isdir(path_in):
-        for file in sorted(os.listdir(path_in)):
-            if file.endswith(extensions):
-                current_file_path = os.path.join(path_in, file)
-                harmonize_crs(current_file_path, ref_filepath)
-                path_out = os.path.join(out_dir_path, file.split('.')[0] + '_aligned.tif')
-                call_arosics(current_file_path, ref_filepath, path_out, corr_type)
-                if dynamic_corr :
-                    ref_filepath = path_out
-                
-    else:
-        raise ValueError(f"The specified path '{path_in}' is not a file nor a directory.")
         
-def complete_arosics_process(path_in, ref_filepath, out_dir_path, corr_type = 'global', dynamic_corr = False, apply_matrix=True, max_shift=250, max_iter=100, window_size=None, mp=0, grid_res=1000):
+def complete_arosics_process(path_in, ref_filepath, out_dir_path, corr_type = 'global', dynamic_corr = False, apply_matrix=True, max_shift=250, max_iter=100, grid_res=1000, window_size=None, mp=0, save_csv = True, save_vector_plot = False):
     assert corr_type in ['global', 'local']
     rm_temp_files = False
     dynamic_corr = str2bool(dynamic_corr)
     apply_matrix = str2bool(apply_matrix)
-    print(bool(apply_matrix))
+    save_vector_plot = str2bool(save_vector_plot)
+    save_csv = str2bool(save_csv)
     #Set default values for window_size
     if corr_type == 'global':
         if window_size is None :
@@ -105,8 +91,6 @@ def complete_arosics_process(path_in, ref_filepath, out_dir_path, corr_type = 'g
         if window_size is None :
             window_size = 4000 
 
-    dynamic_corr = str2bool(dynamic_corr)
-
     extensions = ('.tif', '.tiff', '.TIF', '.TIFF')
 
     if os.path.isfile(path_in):
@@ -115,21 +99,27 @@ def complete_arosics_process(path_in, ref_filepath, out_dir_path, corr_type = 'g
         else:
             harmonize_crs(path_in, ref_filepath)
             path_out = os.path.join(out_dir_path, path_in.split('/')[-1].split('\\')[-1].split('.')[0] + f'_aligned_{corr_type}.tif')
-            call_arosics(path_in, ref_filepath, path_out, corr_type, mp=mp, window_size=window_size, max_shift=max_shift, max_iter=max_iter, grid_res=grid_res)
-
+            CR = call_arosics(path_in, ref_filepath, path_out, corr_type, mp=mp, window_size=window_size, max_shift=max_shift, max_iter=max_iter, grid_res=grid_res, save_vector_plot=save_vector_plot, save_csv=save_csv)
+            return CR
+            
     elif os.path.isdir(path_in):
+        list_CR = []
         files = [file for file in sorted(os.listdir(path_in)) if file.endswith(extensions)]
         print("files : ", files)
 
         if len(files)==0:
             raise ValueError(f"The specified directory '{path_in}' does not contain any GeoTiff files.")
-        
-        elif not apply_matrix:
-            for file in files:
+
+        elif dynamic_corr or not apply_matrix :
+            for i in range(len(files)):
+                file = files[i]
                 current_file_path = os.path.join(path_in, file)
-                harmonize_crs(current_file_path, ref_filepath)
+                harmonize_crs(current_file_path, ref_filepath, check_ref = True if i==0 else False)
                 path_out = os.path.join(out_dir_path, file.split('.')[0].replace("_temp", "") + f'_aligned_{corr_type}.tif')
-                call_arosics(current_file_path, ref_filepath, path_out, corr_type, mp=mp, window_size=window_size, max_shift=max_shift, max_iter=max_iter, grid_res=grid_res)
+                CR = call_arosics(current_file_path, ref_filepath, path_out, corr_type, mp=mp, window_size=window_size, max_shift=max_shift, max_iter=max_iter, grid_res=grid_res)
+                list_CR.append(CR)
+                if dynamic_corr:
+                    ref_filepath = path_out
                 
         else:
             hlist, blist, glist, dlist = [], [], [], []
@@ -141,7 +131,7 @@ def complete_arosics_process(path_in, ref_filepath, out_dir_path, corr_type = 'g
                 glist.append(tf[2])
                 dlist.append(tf[2] + tf[0]*meta['width'])
             
-            if not (all(x == hlist[0] for x in hlist) and all(x == glist[0] for x in glist)):     #and (np.max(dlist)-np.max(glist)) < 0.9*(np.max(dlist)-np.min(glist)) and (np.min(hlist)-np.max(blist)) < 0.9*(np.max(hlist)-np.max(blist))
+            if not (all(x == hlist[0] for x in hlist) and all(x == glist[0] for x in glist)):     #and (np.max(dlist)-np.max(glist)) < 0.9*(np.max(dlist)-np.min(glist)) and (np.min(hlist)-np.max(blist)) < 0.9*(np.max(hlist)-np.max(blist))  #if apply_mask
                 rm_temp_files=True
                 mask_coords = [np.max(glist), np.min(blist), np.max(dlist), np.min(hlist)]
                 geom = Polygon([(mask_coords[0], mask_coords[1]), (mask_coords[0], mask_coords[3]), (mask_coords[2], mask_coords[3]), (mask_coords[2], mask_coords[1])])
@@ -171,40 +161,32 @@ def complete_arosics_process(path_in, ref_filepath, out_dir_path, corr_type = 'g
                             ds_ref.write(red_new_img)
                             ds_ref.close()
                 
-            if dynamic_corr :
-                for file in files:
-                    current_file_path = os.path.join(path_in, file)
-                    harmonize_crs(current_file_path, ref_filepath)
-                    path_out = os.path.join(out_dir_path, file.split('.')[0].replace("_temp", "") + f'_aligned_{corr_type}.tif')
-                    call_arosics(current_file_path, ref_filepath, path_out, corr_type, mp=mp, window_size=window_size, max_shift=max_shift, max_iter=max_iter, grid_res=grid_res)
-                    ref_filepath = path_out
 
             else:
                 first_file = files[0]
                 harmonize_crs(os.path.join(path_in, first_file), ref_filepath)
-                path_out = os.path.join(out_dir_path, first_file.split('.')[0].replace("_temp", "") + f'_aligned_{corr_type}.tif')              
-                CPUs = None if mp else 1
-                if corr_type=='global':
-                    CR = COREG(ref_filepath, os.path.join(path_in, first_file), path_out=path_out, fmt_out="GTIFF", ws=(window_size, window_size), max_shift=max_shift, max_iter=max_iter, CPUs=CPUs)
-                elif corr_type=='local':
-                    CR = COREG_LOCAL(ref_filepath, os.path.join(path_in, first_file), path_out=path_out, fmt_out="GTIFF", window_size=(window_size, window_size), max_shift=max_shift, max_iter=max_iter, CPUs=CPUs, grid_res=grid_res)
-                CR.correct_shifts()
+                path_out = os.path.join(out_dir_path, first_file.split('.')[0].replace("_temp", "") + f'_aligned_{corr_type}.tif')
+                CR = call_arosics(os.path.join(path_in, first_file), ref_filepath, path_out, corr_type, mp=mp, window_size=window_size, max_shift=max_shift, max_iter=max_iter, grid_res=grid_res)             
+                list_CR.append(CR)
                 for file in files[1:]:
                     current_file_path = os.path.join(path_in, file)
                     harmonize_crs(current_file_path, ref_filepath, check_ref=False)
                     path_out = os.path.join(out_dir_path, file.split('.')[0].replace("_temp", "") + f'_aligned_{corr_type}.tif')
-                    DESHIFTER(current_file_path, CR.coreg_info, path_out=path_out, fmt_out="GTIFF").correct_shifts() 
+                    CR = DESHIFTER(current_file_path, CR.coreg_info, path_out=path_out, fmt_out="GTIFF").correct_shifts() 
+                    list_CR.append(CR)
         
         if rm_temp_files:
             for file in files:
                 os.remove(os.path.join(path_in, file))
         
+        return list_CR
+    
     else:
         raise ValueError(f"The specified path '{path_in}' is not a file nor a directory.")
 
 
 if __name__ == '__main__':
-
+    """
     complete_arosics_process(path_in = "//amap-data.cirad.fr/work/users/HadrienTulet/tests_arosics/data", 
                              ref_filepath = "//amap-data.cirad.fr/work/shared/PhenOBS/Paracou/Metashape/RGB_Broad_Mosaics/PARACOU2023_DSM_50cm_filt.tif", 
                              out_dir_path = "//amap-data.cirad.fr/work/users/HadrienTulet/tests_arosics/apply_matrix_loc_no_mask", 
@@ -212,7 +194,7 @@ if __name__ == '__main__':
                              dynamic_corr=False,
                              apply_matrix=True
                              )
-
+    """
 
  
 
