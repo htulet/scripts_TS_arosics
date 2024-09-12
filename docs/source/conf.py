b9c209a93192f64bf676271c6d7677f8a3f60dd1
htulet <hadrien.tulet@cirad.fr>
2024-09-12 11:01:36 +0200

    doc_test+

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import pathlib
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 # -- Project information -----------------------------------------------------
@@ -55,6 +56,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
